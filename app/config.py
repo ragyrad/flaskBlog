145e5719ea8275ca42e0766d725de87d8a1a8f3b
Oleg <urban.oleg1@gmail.com>
2021-03-11 00:10:31 +0300

    Created posts models

--- app/config.py
+++ app/config.py
@@ -1,2 +1,4 @@
 class Configuration:
 	DEBUG = True
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost/test1'
--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from app import app
 import view
 
+from posts.blueprint import posts
+
+app.register_blueprint(posts, url_prefix='/blog')
 
 if __name__ == '__main__':
 	app.run()
